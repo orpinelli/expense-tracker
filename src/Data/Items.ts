ad82dce227db03dd6695817f96d1701f9af53150
GabrielOrpinelli <gabriel.orpinelli@kabum.com.br>
2021-11-25 15:11:43 -0300

    feat: criando list para a home

--- src/Data/Items.ts
+++ src/Data/Items.ts
@@ -1,8 +1,22 @@
 import { Item } from '../types/Item'
 
 export const Items: Item[] = [
-    { date: new Date(2021, 9, 14), category: 'food', title: 'Mc', value: 32.12},
-    { date: new Date(2021, 9, 15), category: 'food', title: 'BK', value: 52.12},
-    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel', value: 1132.12},
-    { date: new Date(2021, 9, 18), category: 'salary', title: 'Salario', value: 3332.12},
-];
+  {
+    date: new Date(2021, 9, 16),
+    category: 'food',
+    title: 'BK',
+    value: 1132.12
+  },
+  {
+    date: new Date(2021, 9, 16),
+    category: 'rent',
+    title: 'Aluguel',
+    value: 1132.12
+  },
+  {
+    date: new Date(2021, 9, 18),
+    category: 'salary',
+    title: 'Salario',
+    value: 3332.12
+  }
+]
